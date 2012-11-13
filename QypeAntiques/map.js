e5d98790ccadf4228be271daaa44d3477ad710a0
Dimitar Ruszev <dimitar.ruszev@gmail.com>
2012-11-13 03:02:40 +0100

    wax update

--- QypeAntiques/map.js
+++ QypeAntiques/map.js
@@ -1,11 +1,10 @@
-//var map;
+var map;
 
 //wax.tilejson('http://api.tiles.mapbox.com/v2/mapbox.geography-class.jsonp',
 	//'http://a.tiles.mapbox.com/v3/diroru.QypeAntiquesTest.jsonp'
-wax.tilejson('http://api.tiles.mapbox.com/v2/diroru.QypeAntiquesTest.jsonp',
+wax.tilejson('http://api.tiles.mapbox.com/v3/diroru.QypeAntiquesTest.jsonp',
   function(tilejson) {
-var map = L.map('map', {
-	center : [52.52, 13.4],
-	zoom : 13
-	});
+    map = new L.map('map')
+      .addLayer(new wax.leaf.connector(tilejson))
+      .setView(new L.LatLng(52.52, 13.4), 13);
 });
